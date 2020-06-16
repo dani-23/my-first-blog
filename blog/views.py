b391caa4988682baaaf17791e8be69f723c00751
danny3l <danielstoica2329@gmail.com>
2020-06-16 12:28:43 +0300

    Delete + confirmation delete feature added

--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,16 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib import messages
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    
     return render(request, 'blog/post_details.html', {'post': post})
 
 def post_new(request):
@@ -20,13 +23,13 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('post_list')
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+    post = get_object_or_404(Post, pk =pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
@@ -38,3 +41,11 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def confirm_delete(request, pk):
+    post = get_object_or_404(Post, pk =pk)
+    if request.method == "POST":
+        post.delete()
+        messages.success(request, "This has been deleted")
+        return redirect('post_list')
+    return render(request, 'confirm_delete.html', {'post':post})
